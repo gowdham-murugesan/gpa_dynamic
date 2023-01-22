1ab4e9039ba1ea7160c7de3731f949dd9642e07f
Gowdham2004 <gowdhammurugesh24@gmail.com>
2023-01-22 18:05:43 +0530

    animated

--- index.js
+++ index.js
@@ -264,7 +264,7 @@ function GPA() {
   // console.log(sumGPA / sumCredit);
   document.getElementById("gpa").innerHTML = "GPA : " + (sumGPA / sumCredit).toFixed(2);
   // document.getElementById("gpadiv").style.display = "block";
-  $("#gpadiv").fadeIn(2500);
+  $("#gpadiv").fadeIn(3000);
 
 
 
@@ -357,7 +357,7 @@ function GPA() {
   // Append the table to the document
   tablediv.appendChild(table);
   $("#tablediv").hide();
-  $("#tablediv").show(100);
+  $("#tablediv").show(1000);
   $("html, body").animate({ scrollTop: $(document).height() });
 }
 
